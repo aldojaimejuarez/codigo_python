d62f99cb9abd6f7b7e04872af5b64d5eefc0ebbe
aldojaimejuarez <aldojaimejuarez@gmail.com>
2020-10-07 18:50:23 -0500

    Comparando simplificación

--- palindromo.py
+++ palindromo.py
@@ -1,23 +1,45 @@
-def palindromo(palabra):
-    palabra = palabra.replace(" ", "")
-    palabra = palabra.lower()
-    palabra_invertida = palabra[::-1]  
-    if palabra == palabra_invertida:
+# def palindromo(palabra):
+#     palabra = palabra.replace(" ", "")
+#     palabra = palabra.lower()
+#     palabra_invertida = palabra[::-1]  
+#     if palabra == palabra_invertida:
+#         return True
+#     else:
+#         return False
+
+
+# def run():
+#     palabra = input("Escribe una palabra: ")
+#     es_palindromo = palindromo(palabra)
+#     if es_palindromo == True:
+#         print("Es Palíndromo")
+#     else:
+#         print("No es Palíndromo")
+
+
+# if __name__ == "__main__":
+#     run()
+
+
+
+def es_palindromo(palabra):
+    palabra = palabra.replace(' ', '').lower()
+    
+    if palabra[::] == palabra[::-1]:
         return True
+    
     else:
         return False
 
 
 def run():
-    palabra = input("Escribe una palabra: ")
-    es_palindromo = palindromo(palabra)
-    if es_palindromo == True:
-        print("Es Palíndromo")
+    palabra = input('Ingrese una palabra: ')
+    if es_palindromo(palabra):
+        print('Es palindromo')
+
     else:
-        print("No es Palíndromo")
+        print('No es palindromo')
 
 
 if __name__ == "__main__":
     run()
-
-
