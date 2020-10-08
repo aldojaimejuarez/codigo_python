c04ec87e9580800dc1dfa2e9fdccb85bdb7841a6
aldojaimejuarez <aldojaimejuarez@gmail.com>
2020-10-07 23:12:52 -0500

    while

--- ciclos.py
+++ ciclos.py
@@ -1,16 +1,16 @@
-def run():
-    LIMITE = 1000
+# def run():
+#     LIMITE = 1000
 
-    contador = 0
-    potencia_2 = 2**contador
-    while potencia_2 < LIMITE:
-        print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2))
-        contador = contador + 1
-        potencia_2 = 2**contador
+#     contador = 0
+#     potencia_2 = 2**contador
+#     while potencia_2 < LIMITE:
+#         print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2))
+#         contador = contador + 1
+#         potencia_2 = 2**contador
 
 
-if __name__ == '__main__':
-    run() 
+# if __name__ == '__main__':
+#     run() 
 
 #     def run():
 #     LIMITE = 10
@@ -26,3 +26,9 @@ if __name__ == '__main__':
 
 # if __name__ == '__main__':
 #     run()
+
+contador = 1
+print(contador)
+while contador < 10:
+    contador += 1
+    print(contador)
